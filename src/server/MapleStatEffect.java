fa17b3212cb1eba8cc65bf0bda4904225b2118a2
reanox <reanox@hotmail.com.tw>
2017-09-20 17:20:33 +0800

    修正卷軸無法在村莊使用

--- src/server/MapleStatEffect.java
+++ src/server/MapleStatEffect.java
@@ -946,7 +946,7 @@ public class MapleStatEffect implements Serializable {
 
     public final boolean applyReturnScroll(final MapleCharacter applyto) {
         if (moveTo != -1) {
-            if (applyto.getMap().getReturnMapId() != applyto.getMapId()) {
+            //if (applyto.getMap().getReturnMapId() != applyto.getMapId()) {
                 MapleMap target;
                 if (moveTo == 999999999) {
                     target = applyto.getMap().getReturnMap();
@@ -962,7 +962,7 @@ public class MapleStatEffect implements Serializable {
                 }
                 applyto.changeMap(target, target.getPortal(0));
                 return true;
-            }
+            //}
         }
         return false;
     }
